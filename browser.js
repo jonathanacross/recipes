23db6cdb6f26d23eb74931c96f79ef75197d2a37
Jonathan Cross <jonathan.a.cross@gmail.com>
2023-09-04 15:39:47 -0700

    adding selection of recipes

--- browser.js
+++ browser.js
@@ -49,21 +49,49 @@ function ParseRecipes(responses) {
     recipe_data = responses.map(x => ParseMarkdown(x));  // TODO: global variable fishiness
 }
 
-function MakeHtmlData(recipe) {
-    let rDiv = document.createElement("div");
-    rDiv.className = "recipe";
+function MakeResultList(resultids, recipes) {
+    let result_div = document.getElementById("resultlist");
+    let result_list = document.createElement("ul");
+    result_div.appendChild(result_list);
+
+    for (id of resultids) {
+        let item = document.createElement("li");
+        let recipe = recipes[id];
+        item.innerText = recipe.name;
+        item.id = id;
+        item.addEventListener('click', function (e) {
+            SelectResult(item.id, recipe);
+        });
+        result_list.appendChild(item);
+    }
+}
+
+function SelectResult(id, recipe) {
+    var idxelem = document.getElementById(id);
+
+    Array.prototype.filter.call(idxelem.parentNode.children, function (siblings) {
+                        siblings.className = 'unselected';
+    });
+    idxelem.className = 'selected';
+
+    ShowRecipe(recipe);
+}
+
+function ShowRecipe(recipe) {
+    let resultDiv = document.createElement("div");
+    resultDiv.id = 'recipe';
 
     let nameHeader = document.createElement("h1");
     nameHeader.innerText = recipe.name;
-    rDiv.appendChild(nameHeader);
+    resultDiv.appendChild(nameHeader);
 
     let metaPara = document.createElement("p");
     metaPara.innerText = recipe.meta.join(" ");
-    rDiv.appendChild(metaPara);
+    resultDiv.appendChild(metaPara);
 
     let ingredientsHeader = document.createElement("h2");
     ingredientsHeader.innerText = "Ingredients";
-    rDiv.appendChild(ingredientsHeader);
+    resultDiv.appendChild(ingredientsHeader);
 
     let ingredientsPara = document.createElement("p");
     for (ingredient of recipe.ingredients) {
@@ -71,26 +99,29 @@ function MakeHtmlData(recipe) {
         ingredientsPara.appendChild(document.createElement('br'));
         var br = document.createElement("br");
     }
-    rDiv.appendChild(ingredientsPara);
+    resultDiv.appendChild(ingredientsPara);
 
     let directionsHeader = document.createElement("h2");
     // TODO: handle paragraph breaks, (and other markdown formatting?)
     directionsHeader.innerText = "Directions";
-    rDiv.appendChild(directionsHeader);
+    resultDiv.appendChild(directionsHeader);
 
     let directionsPara = document.createElement("p");
     directionsPara.innerText = recipe.directions.join(" ");
-    rDiv.appendChild(directionsPara);
+    resultDiv.appendChild(directionsPara);
      
-    return rDiv;
+    let oldResult = document.getElementById("recipe");
+    oldResult.replaceWith(resultDiv);
 }
 
 function SetupPage() {
-    let results = document.getElementById("results");
-    for (recipe of recipe_data) {
-        let div = MakeHtmlData(recipe);
-        results.appendChild(div);
-    }
+    // temporary hack: list of indices of recipes to return
+    let resultids = [0, 1];
+
+    MakeResultList(resultids, recipe_data);
+
+    let recipe = recipe_data[resultids[0]];
+    ShowRecipe(recipe);
 }
 
 function ReadRecipes() {
@@ -98,7 +129,6 @@ function ReadRecipes() {
       .then(result => Promise.all(result.map(v => v.text())))
       .then(results => ParseRecipes(results))
       .then(x => SetupPage(x));
-
 }
 
 window.onload = ReadRecipes;
